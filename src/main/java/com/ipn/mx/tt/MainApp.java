97ed9381b574714004d50de888598a8b4449838b
Tuky88 <axelreyezamezcua@gmail.com>
2019-01-29 00:12:27 -0600

    Primer Avance

---
+++ src/main/java/com/ipn/mx/tt/MainApp.java
@@ -0,0 +1,40 @@
+package com.ipn.mx.tt;
+
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+        
+        Image image = new Image("/imagenes/brain.png");
+        stage.getIcons().add(image);
+        stage.setTitle("TT 2018-A030");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
