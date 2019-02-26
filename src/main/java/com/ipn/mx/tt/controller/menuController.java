013c8da9c19cb92bba46fd75db7cb2c286de87a0
Axel Reyes <Axel Reyes@10.100.67.38>
2019-02-26 17:53:05 -0600

    primera prueba hilo reloj

--- src/main/java/com/ipn/mx/tt/controller/menuController.java
+++ src/main/java/com/ipn/mx/tt/controller/menuController.java
@@ -8,11 +8,14 @@ package com.ipn.mx.tt.controller;
 import com.ipn.mx.tt.util.AlertMessage;
 import com.ipn.mx.tt.util.movEscena;
 import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
 /**
@@ -34,10 +37,16 @@ public class menuController implements Initializable {
     private Button btnCerrarSesion;
 
     @FXML
+    private Label lblHora;
+
+    @FXML
+    private Label lblFecha;
+
+    @FXML
     void cerrarSesion(ActionEvent event) {
-        int resp=alertMessage.confirm(0, "¿Cerrar Sesión?","Desea cerrar sesión");
-        if(resp==1){
-         mov.cambiarEscena(event, "Scene.fxml");
+        int resp = alertMessage.confirm(0, "¿Cerrar Sesión?", "Desea cerrar sesión");
+        if (resp == 1) {
+            mov.cambiarEscena(event, "Scene.fxml");
         }
     }
 
@@ -48,7 +57,36 @@ public class menuController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         mov = new movEscena();
-        alertMessage=new AlertMessage();
+        alertMessage = new AlertMessage();
+        runClock();
+    }
+
+    public void runClock() {
+
+        boolean running = true;
+        new Thread() {
+            public void run() {
+                long last = System.nanoTime();
+                double delta = 0;
+                double ns = 1000000000.0;
+                while (running) {
+                    long now = System.nanoTime();
+                    delta += (now - last) / ns;
+                    last = now;
+                    while (delta >= 1) {
+
+                        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy---HH:mm:ss");
+                        LocalDateTime ahorita;
+                        ahorita = LocalDateTime.now();
+                        String fecha[] = dtf.format(ahorita).split("---");
+                        lblFecha.setText(fecha[0]);
+                        lblFecha.setText(fecha[1]);
+                        delta--;
+                    }
+                }
+            }
+        }.start();
+
     }
 
 }
