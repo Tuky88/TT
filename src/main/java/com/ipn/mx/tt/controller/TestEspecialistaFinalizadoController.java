77cfbd264902c191e634ea00af8ad3b5f8c648fa
Angel <angel@10.100.12.205>
2019-03-10 23:00:37 -0600

    Merge origin/master
    Conflicts:
    	src/main/java/com/ipn/mx/tt/MainApp.java
    	src/main/resources/fxml/FXML.fxml

--- src/main/java/com/ipn/mx/tt/controller/TestEspecialistaFinalizadoController.java
+++ src/main/java/com/ipn/mx/tt/controller/TestEspecialistaFinalizadoController.java
@@ -10,47 +10,24 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ScrollBar;
-import javafx.scene.control.TableView;
 import javafx.scene.layout.Pane;
-import javafx.scene.media.MediaView;
 
 /**
  * FXML Controller class
  *
  * @author garci
  */
-public class TestEspecialistaController implements Initializable {
-@FXML
-    private Label lblTEpregunta;
+public class TestEspecialistaFinalizadoController implements Initializable {
+
+     @FXML
+    private Button btnTEprediagnostico;
 
     @FXML
-    private RadioButton rbtnTEopcion3;
-
-    @FXML
-    private Button btnTEsiguiente;
-
-    @FXML
-    private RadioButton rbtnTEopcion2;
-
-    @FXML
-    private RadioButton rbtnTEopcion1;
-
-    @FXML
-    private ProgressBar pbTEprogeso;
+    private ProgressBar pbTEterminado;
 
     @FXML
     private Pane panelLeft1;
-
-     @FXML
-    private TableView<?> listTEpreguntas;
-
-    @FXML
-    private ScrollBar sbTEpreguntas;
-
     /**
      * Initializes the controller class.
      */
