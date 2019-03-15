56951322a09366d974692e4eb3dd688b9d5f905f
Angel <angel@10.100.12.205>
2019-03-14 18:59:22 -0600

    Merge origin/master
    Conflicts:
    	src/main/java/com/ipn/mx/tt/MainApp.java
    	src/main/resources/fxml/FXML.fxml

---
+++ src/main/java/com/ipn/mx/tt/controller/HistorialController.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ipn.mx.tt.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author garci
+ */
+public class HistorialController implements Initializable {
+  @FXML
+    private TextField txtPnombre;
+
+    @FXML
+    private TableView<?> tblPpre;
+
+    @FXML
+    private Button btnPver;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
