46043784045cd6bc5e3f8aaa54e569443a20dbdd
Angel <angel@10.100.12.205>
2019-04-01 19:35:19 -0600

    Merge origin/master
    Conflicts:
    	src/main/java/com/ipn/mx/tt/MainApp.java
    	src/main/resources/fxml/FXML.fxml

--- src/main/java/com/ipn/mx/tt/controller/LoginController.java
+++ src/main/java/com/ipn/mx/tt/controller/LoginController.java
@@ -66,7 +66,7 @@ public class LoginController implements Initializable {
 
                 //alertMessage.alert(0, "BIENVENIDO AL SISTEMA", "BIENVENIDO: " + user.getId());
             } else {
-                lblStatus.setText("inBIENVENIDO");
+                //lblStatus.setText("inBIENVENIDO");
 
             }
         } else {
