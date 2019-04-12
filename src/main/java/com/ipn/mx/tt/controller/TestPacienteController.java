f94e544256604b4f94a473a12a0eeb3d46765e95
Axel Reyes <34036239+Tuky88@users.noreply.github.com>
2019-04-11 22:23:33 -0500

    Ultimo

--- src/main/java/com/ipn/mx/tt/controller/TestPacienteController.java
+++ src/main/java/com/ipn/mx/tt/controller/TestPacienteController.java
@@ -13,10 +13,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
