bca8f31aed4a69c0dccc78e096d8a86c6f3aee53
Axel Reyes <34036239+Tuky88@users.noreply.github.com>
2019-04-19 23:59:01 -0500

    VERSION OPTIMIZADA DEL CUESTIONARIO, ESPECALISTA

--- src/main/java/com/ipn/mx/tt/controller/TestEspecialistaController.java
+++ src/main/java/com/ipn/mx/tt/controller/TestEspecialistaController.java
@@ -240,11 +240,11 @@ public class TestEspecialistaController implements Initializable {
 
     private void sumarATrastorno() {
         sintoma = test.getSintoma(pregunta);
-        System.out.println("Sintomas:" + sintoma.size());
+  //      System.out.println("Sintomas:" + sintoma.size());
         //System.out.println(sintoma.toString());
         sintoma.forEach((sintomaLoop) -> {
             SintomaPregunta sp = (SintomaPregunta) sintomaLoop;
-            System.out.println(sp.toString());
+//            System.out.println(sp.toString());
             int numeroSintoma = sp.getSintoma();
             trastorno = test.getTrastorno(numeroSintoma);
             trastorno.forEach((trastornoLoop) -> {
